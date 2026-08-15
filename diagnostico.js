49485bd718b9cb05c91699723907ad929f2cee1c
alupizarro <aannpp1974@gmail.com>
2026-08-15 19:41:31 -0300

    Crear proyecto inicial

---
+++ diagnostico.js
@@ -0,0 +1,5 @@
+console.log("Diagnóstico del entorno");
+console.log("Versión de Node.js:", process.version);
+console.log("Plataforma:", process.platform);
+console.log("Argumentos:", process.argv);
+console.log("Variable de entorno NOMBRE:", process.env.NOMBRE);
